0a8a269dead92d028f0074035b79e187a3170ae5
jfraser2 <69680391+jfraser2@users.noreply.github.com>
2026-02-09 22:36:43 -0600

    first version for check-in

---
+++ src/com/speechify/TestAssessment.java
@@ -0,0 +1,22 @@
+package com.speechify;
+
+public class TestAssessment {
+
+	public static void main (String [] args)
+	{
+		TestAssessment myApp = new TestAssessment();
+		myApp.run();
+		
+		System.out.println("Usage - TestAssessment ");
+	}
+	
+	public TestAssessment() {
+	}
+	
+	public void run() {
+//		DecodeMessage decodeMessage = new DecodeMessage();
+//		decodeMessage.messageDecoder(testUrl);
+//		decodeMessage.messageDecoder("");
+	}
+	
+}
